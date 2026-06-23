a21c6474c0d03cb26cf206f0c857f3fa9e3a472d
Moha Gayathri <mohagayathrik@gmail.com>
2026-06-23 11:21:42 +0000

    add jobs REST API endpoints

--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -14,13 +14,40 @@ router.post("/",async (req,res)=>{
                 payload:payload || {}
             }
         });
-        res.status(201).json(newJob);
+        res.status(201).json({id:newJob.id,status:newJob.status});
     } catch(err) {
         console.error("Database save error",err);
         res.status(500).json({error:"Internal Server Error", message:"Failed to save record"});
     }
 });
-// router.get("/:id",(req,res)=>{});
-// router.get("/",(req,res)=>{})
+router.get("/:id",async (req,res)=>{
+    const jobId = req.params.id;
+    try {
+        const job = await prisma.job.findUnique({
+            where: {
+                id: jobId
+            }
+        });
+        if(!job) {
+            return res.status(404).json({error:"Job not found"});
+        }
+        res.status(200).json({id:job.id,status:job.status,type:job.type,payload:job.payload,result:job.result,error:job.error});
+    } catch(err) {
+        console.error("Failed to fetch",err);
+        res.status(500).json({error:"Internal Server Error"})
+    }
+});
+router.get("/",async (req,res)=>{
+    try{
+        const allJobs = await prisma.job.findMany({orderBy: {createdAt:'desc'},take:50});
+        if(allJobs.length === 0) {
+            return res.status(404).json({error:"Jobs not found"});
+        }
+        res.status(200).json(allJobs);
+    } catch(err) {
+        console.error("Failed to fetch",err);
+        res.status(500).json({error:"Internal Server Error"});
+    }
+});
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 8000;
 const jobsRouter = require('./routes/jobs');
-console.log(jobsRouter);
+
 app.use(express.json())
 app.use('/jobs',jobsRouter);
 
